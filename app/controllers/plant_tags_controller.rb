a350c6e83dadc81b05826f2b1245990810887f58
Francesca Santoriello <f.santoriello@hotmail.com>
2023-02-23 19:55:39 +0100

    refactor

--- app/controllers/plant_tags_controller.rb
+++ app/controllers/plant_tags_controller.rb
@@ -15,6 +15,25 @@ class PlantTagsController < ApplicationController
             PlantTag.create(plant: @plant, tag_id: id)
         end
         
-        redirect_to garden_path(@garden)
+
     end
+
+    def create
+        plant = Plant.find(params[:plant_id])
+        tag = Tag.find(plant_tag_params[:tag])
+    
+        @p_tag = PlantTag.new(plant: plant, tag: tag)
+    
+        if @plant_tag.save
+          redirect_to garden_path(plant.garden)
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+    
+      private
+    
+      def plant_tag_params
+        params.require(:plant_tag).permit(:tag)
+      end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+    const banner = document.getElementById('banner');
+    const arrowRight = document.getElementById('right');
+    
+    arrowRight.addEventListener('click', (e) => {
+        banner.classList.add('newImage')
+    });
+
